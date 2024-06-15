7be58a4a5779279f143a252955ef63d23f65d2ae
Mateus-Saantos <154933741+Mateus-Saantos@users.noreply.github.com>
2024-06-14 21:50:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// Configuração do Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAepcCOElS6km3rOas8cHA93sEsn7Xhqk0",
+    authDomain: "tester-76381.firebaseapp.com",
+    databaseURL: "https://tester-76381-default-rtdb.firebaseio.com",
+    projectId: "tester-76381",
+    storageBucket: "tester-76381.appspot.com",
+    messagingSenderId: "489689030181",
+    appId: "1:489689030181:web:c9081f73a85f18659929cc"
+  };
+
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
