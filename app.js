83f23daf6ced3fa6f35631650d392fbc94476e3e
Mateus-Saantos <154933741+Mateus-Saantos@users.noreply.github.com>
2024-06-17 19:31:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Configuração do Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyAepcCOElS6km3rOas8cHA93sEsn7Xhqk0",
-    authDomain: "tester-76381.firebaseapp.com",
-    databaseURL: "https://tester-76381-default-rtdb.firebaseio.com",
-    projectId: "tester-76381",
-    storageBucket: "tester-76381.appspot.com",
-    messagingSenderId: "489689030181",
-    appId: "1:489689030181:web:c9081f73a85f18659929cc"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDsi3quAxlYIL4HKpijhj8e0qCclPXPuVo",
+  authDomain: "github-c0ac9.firebaseapp.com",
+  databaseURL: "https://github-c0ac9-default-rtdb.firebaseio.com",
+  projectId: "github-c0ac9",
+  storageBucket: "github-c0ac9.appspot.com",
+  messagingSenderId: "886174193200",
+  appId: "1:886174193200:web:95e2061811c55c6a01fff5",
+  measurementId: "G-3HY7J8ZBRT"
+};
 
-// Inicialize o Firebase
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
